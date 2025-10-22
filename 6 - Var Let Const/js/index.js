099b214a257e0f08f935f92c6db8e98ef19ba579
Elaine  Santana <elaindi@gmail.com>
2025-10-22 11:03:17 -0300

    Add files via upload

---
+++ 6 - Var Let Const/js/index.js
@@ -0,0 +1,33 @@
+/* Vamos entender variáveis
+variáveis são "recipientes" onde podemos armazenar informações que podem variar, ou seja, podem ter qualquer tipo de valor.
+no Javacript temos 3 palavras-chaves para declarar variáveis
+-> var
+-> let
+-> const
+*/
+
+//declaração de variáveis
+var a,b,c;
+
+//atribuição de valores
+a = 2;
+b = 3;
+c = a + b;
+
+alert(c);
+
+var nome, sobrenome, nomeCompleto;
+/*atribuição de valores
+nome = "Elaine";
+sobrenome = "Santana";
+nomeCompleto = nome + " " + sobrenome;
+
+document.getElementById("texto").innerHTML = nomeCompleto;
+*/
+const x = 10;
+
+document.getElementById("texto").innerHTML = x;
+
+
+
+
